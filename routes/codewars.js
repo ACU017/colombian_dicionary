64f1cc4c8c0c17dd8564eaffd273ac39bc84e69c
Andrea <anfrescortes@gmail.com>
2023-08-08 10:49:26 +0200

    words.js updated

--- routes/codewars.js
+++
@@ -1,9 +0,0 @@
-function calculamesta(n) {
-  let sum = 0;
-  for (let i = 0; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      sum += i;
-    }
-    return sum;
-  }
-}
---
+++ routes/words.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../model/helper");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  db("SELECT * FROM words;")
+    .then((results) => {
+      res.send(results.data);
+    })
+    .catch((err) => res.status(500).send(err));
+});
+
+module.exports = router;
