546ef12630072d570d314227c6c451a79cf18e69
Andrea <anfrescortes@gmail.com>
2023-08-10 11:29:45 +0200

    separated searchbar component

--- client/src/components/Dictionary.jsx
+++ client/src/components/Dictionary.jsx
@@ -1,59 +1,10 @@
 import React, { useState } from "react";
+import Searchbar from "./Searchbar";
 
 function Dictionary() {
-  const [searchResult, SetSearchResult] = useState([]);
-  const [searchInput, SetSearchInput] = useState("");
-
-  const handleChange = (e) => {
-    e.preventDefault();
-    SetSearchInput(e.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    searchWord(searchInput);
-  };
-  const searchWord = async () => {
-    try {
-      const response = await fetch(`/api/words/${searchInput}`, {
-        method: "GET",
-      });
-
-      const json = await response.json(); // should I transform everything into a small caps ?
-      SetSearchResult(json);
-    } catch (error) {
-      console.log("errorz");
-    }
-  };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Busque aquí "
-          onChange={handleChange}
-          value={searchInput}
-        />
-        <button type="submit">Dele</button>
-      </form>
-      {searchResult.length === 0
-        ? ""
-        : searchResult
-            .slice()
-            .sort((a, b) => a.word.localeCompare(b.word))
-            .map((result) => {
-              return (
-                <div key={result.id}>
-                  <h5> {result.word}</h5>
-                  <p>
-                    Categoría: <i>{result.category}</i>
-                  </p>
-                  <p> Definición : {result.definition_es}</p>
-                  <p> Ejemplos : {result.example_1}</p>
-                  <p>{result.example_2}</p>
-                </div>
-              );
-            })}
+      <Searchbar />
     </div>
   );
 }
--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,3 +1,60 @@
+// Import all the component from REACT
+import { useState } from "react";
+
 export default function Searchbar() {
-  return <div></div>;
+  const [searchResult, SetSearchResult] = useState([]);
+  const [searchInput, SetSearchInput] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    SetSearchInput(e.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchWord(searchInput);
+  };
+  const searchWord = async () => {
+    try {
+      const response = await fetch(`/api/words/${searchInput}`, {
+        method: "GET",
+      });
+
+      const json = await response.json(); // should I transform everything into a small caps ?
+      SetSearchResult(json);
+    } catch (error) {
+      console.log("errorz");
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Busque aquí "
+          onChange={handleChange}
+          value={searchInput}
+        />
+        <button type="submit">Dele</button>
+      </form>
+      {searchResult.length === 0
+        ? ""
+        : searchResult
+            .slice()
+            .sort((a, b) => a.word.localeCompare(b.word))
+            .map((result) => {
+              return (
+                <div key={result.id}>
+                  <h5> {result.word}</h5>
+                  <p>
+                    Categoría: <i>{result.category}</i>
+                  </p>
+                  <p> Definición : {result.definition_es}</p>
+                  <p> Ejemplos : {result.example_1}</p>
+                  <p>{result.example_2}</p>
+                </div>
+              );
+            })}
+    </div>
+  );
 }
